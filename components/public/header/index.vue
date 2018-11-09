1dc478b309bee968d9f01b8b8c1df842eb4a8e3a
zhou <1@.com>
2018-11-09 09:40:38 +0800

    init

---
+++ components/public/header/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-row :gutter="15" class="m-header">
+    <el-col :span="6">Geo/></el-col>
+    <el-col :span="6">
+      <div class="grid-content bg-purple" />
+    </el-col>
+    <el-col :span="6">
+      <div class="grid-content bg-purple" />
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import Geo from './geo.vue'
+export default {
+  components: {
+    Geo
+  }
+}
+</script>
+
+<style  lang="scss">
+// @import "@assets/css/public/layout.css"
+</style>
